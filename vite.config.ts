6e6d86bfb788ccbf5a9bb3c5a4d8b5bd5c47fc5f
Adam Vosburgh <adamvosburgh@gmail.com>
2026-03-07 13:29:51 -0500

    moved to new domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kallax-configurator/',
+  base: '/',
   plugins: [react()],
   test: {
     globals: true,
